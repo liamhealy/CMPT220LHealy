58ded9e3c225b61710b33e4bd7df5e1bf2083b44
Liam Healy <liam.healy1@marist.edu>
2016-09-01 20:42:31 -0400

    This is my second commit, Project 0, for SD1.

---
+++ Projects/driver_proj0/src/driver_proj0.java
@@ -0,0 +1,10 @@
+package driver_proj0;
+
+public class driver_proj0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello World!");
+	}
+
+}
