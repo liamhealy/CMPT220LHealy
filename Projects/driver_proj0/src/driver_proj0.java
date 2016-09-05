569897244fdec47ef4b43ef2f089b7a4edf326d6
liamhealy <liam.healy1@marist.edu>
2016-09-05 19:57:37 -0400

    Update driver_proj0.java

--- Projects/driver_proj0/src/driver_proj0.java
+++ Projects/driver_proj0/src/driver_proj0.java
@@ -1,3 +1,13 @@
+/*
+ * File: driver_proj0.java
+ * Creator: Liam Healy
+ * course: CMPT 220L
+ * Assignment: Project 0
+ * Due Date:
+ * version: 1.8
+ * 
+ * This file contains a main method that outputs a simple greeting.
+*/
 public class driver_proj0 {
 
 	public static void main(String[] args) {
