ca226b4a20908f7d8b0cded0e9adf9dd58e7dabf
liamhealy <liam.healy1@marist.edu>
2016-09-28 20:08:33 -0400

    Update smallestInt.java

--- Labs/Lab3/smallestInt.java
+++ Labs/Lab3/smallestInt.java
@@ -1,4 +1,13 @@
-
+/**
+ * file: smallestInt.java
+ * author: Liam Healy
+ * course: CMPT 220
+ * assignment: Lab 3
+ * due date: September 22nd, 2016
+ * version: 1.8
+ * 
+ * This file determines the largest possible double, n, such that n^2 is less than 12000.
+ */
 public class smallestInt {
   public static void main(String[] args){
     double n = 0;
