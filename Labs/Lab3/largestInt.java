918029b169e1d0d2219ce2a03aa23b97cf1098e3
liamhealy <liam.healy1@marist.edu>
2016-09-28 20:06:11 -0400

    Update largestInt.java

--- Labs/Lab3/largestInt.java
+++ Labs/Lab3/largestInt.java
@@ -1,3 +1,13 @@
+/**
+ * file: largestInt.java
+ * author: Liam Healy
+ * course: CMPT 220
+ * assignment: Lab 3
+ * due date: September 22nd, 2016
+ * version: 1.8
+ * 
+ * This file determines the largest possible double such that double^3 is still greater than 12000.
+ */
 public class largestInt{
   public static void main(String[] args){
     double n = 12000;
